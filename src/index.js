e589caa6013f37fe7d3d200ddc4665dbb861536d
savinps <savinps@gmail.com>
2019-12-10 19:13:54 -0500

    configuring socket connection

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+let count = 0;
+
+//server (emit) -> client (receive) - countUpdated
+//client (emit) -> server(receive) - increment
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+    socket.emit('message', 'Welcome!');
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server in up on port: ${port}`);
+})
+

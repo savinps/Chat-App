d56663cfddef3aec8773b181cf993d4b40f4d9d4
savinps <savinps@gmail.com>
2019-12-10 23:10:19 -0500

    Integrated geolocation and chat template

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,72 @@
 const socket = io()
 
+// server(emit) -> client(receive) --acknowledgement --> server
+// client(emit) -> server(receive) --acknowledgment --> client
+
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html = Mustache.render(locationMessageTemplate, {
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
+    //disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
     const message = e.target.elements.message.value;
-    socket.emit('sendMessage', message);
+
+    socket.emit('sendMessage', message, (error) => {
+        //enable
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if(error) {
+            return console.log(error)
+        }
+
+        console.log("This message was delivered ")   
+    });
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log("Location shared")
+        })
+    })
+
 })
